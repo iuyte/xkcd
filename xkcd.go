07f7912d3a98c47c2855cb8616fa7c17b2829fbb
iuyte <iuyte@gmx.com>
2017-09-16 17:37:58 -0400

    Made token path relative

--- xkcd.go
+++ xkcd.go
@@ -104,7 +104,7 @@ func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
 }
 
 func Token() string {
-	b, err := ioutil.ReadFile("/token.txt")
+	b, err := ioutil.ReadFile("./token.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
